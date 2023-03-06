969e141a0a357a3d6358a45f2a9b6592b97bca28
rajnishramaera <rajnish.it@ramaera.in>
2023-03-06 10:08:55 +0530

    done/ except font

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
-import "./App.css"
+import { useState } from "react"
+
 const App = () => {
+  const [digit, setDigit] = useState(1000)
+  const [words, setWords] = useState("ONE THOUSAND")
+  const commafy = (num) => {
+    if (num === 1000) {
+      return "1,000"
+    }
+    if (num === 10000) {
+      return "10,000"
+    }
+    if (num === 100000) {
+      return "1,00,000"
+    }
+  }
   return (
-    <div style={{ padding: "5rem" }}>
-      <div className="bg-holder" style={{ zIndex: "-2" }}>
-        <img className="background" src="./src/assets/logo.png" alt="" />
-      </div>
-      <h1 style={{ fontSize: "8rem", color: "orange" }}>
+    <div className="wrapper">
+      <h1
+        style={{
+          width: "1000px",
+          fontSize: "5.6rem",
+          color: "orange",
+          textAlign: "left",
+        }}
+      >
         RAMAERA INDUSTRIES LTD
       </h1>
-      <h2 style={{ fontSize: "3rem", padding: " 2rem 0" }}>
+      <h2 style={{ fontSize: "2.4rem", padding: " 2.4rem 0" }}>
         Received From ___________________
       </h2>
-      <h2 style={{ fontSize: "3rem", padding: " 2rem 0" }}>
-        We have Received the sum of rupee 1,000/- (ONE THOUSAND ONLY), for your
-        common KYC
+      <h2 style={{ fontSize: "2.6rem", padding: " 2rem 0 0 0" }}>
+        We have Received the sum of rupee{" "}
+        <span style={{ color: "red" }}>
+          {commafy(digit)}/- ({words} ONLY)
+        </span>
+        ,
+      </h2>
+      <h2 style={{ fontSize: "2.6rem", padding: "  0 0 2rem 0" }}>
+        for your common KYC
       </h2>
       <h3
         style={{
+          width: "780px",
           color: "white",
-          fontSize: "4rem",
-          padding: " 2rem 0",
+          fontSize: "2.8rem",
+          padding: " 0.6rem 0",
           backgroundColor: "green",
           textAlign: "center",
           border: "solid 2px black",
@@ -27,15 +52,30 @@ const App = () => {
       >
         YOUR KYC HAS BEEN SUCCESSFULLY APPROVED{" "}
       </h3>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "right",
+          flexDirection: "column",
+          margin: " 0 0 0 600px",
+        }}
+      >
         <img
           style={{
             width: "200px",
+            transform: "scale(80%)",
           }}
           src="./src/assets/stamp.png"
           alt=""
         />
-        <span style={{ width: "200px", textAlign: "center" }}>
+        <span
+          style={{
+            width: "200px",
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "1.8rem",
+          }}
+        >
           Authorized Stamp
         </span>
       </div>
